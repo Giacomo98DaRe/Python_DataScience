c2c744b7a2c0f794188de1ac214acecd3c01c2bb
Giacomo Da Re <giacomodare98@gmail.com>
2025-12-01 22:58:25 +0100

    Update GenericReader with encoding argument

--- utilities/reader_tools/GenericCSVReader.py
+++ utilities/reader_tools/GenericCSVReader.py
@@ -1,11 +1,14 @@
 import os
 import pandas as pd
 
-def DataframeFromCSV(csv_name):
+def DataframeFromCSV(csv_name, encoding=None):
     
     os.getcwd()
     csv_path = os.path.join(os.getcwd(),"..", "..", "data", csv_name)
-
-    df = pd.read_csv(csv_path)
+    
+    if(encoding):
+        df = pd.read_csv(csv_path, encoding=encoding)
+    else:
+        df = pd.read_csv(csv_path)
     
     return df

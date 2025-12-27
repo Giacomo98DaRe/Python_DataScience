f9192bef6fbb51dd3c788ec8a2c3263b197ca9bd
Giacomo Da Re <giacomodare98@gmail.com>
2025-12-27 18:50:11 +0100

    Finish random sample notebook. Update genericCSVreader with set index option and absolute path handling.

--- utilities/reader_tools/GenericCSVReader.py
+++ utilities/reader_tools/GenericCSVReader.py
@@ -1,14 +1,18 @@
 import os
 import pandas as pd
 
-def DataframeFromCSV(csv_name, encoding=None):
+def DataframeFromCSV(csv_name, encoding=None, setIndexName=None):
+    base_path = os.path.dirname(os.path.abspath(__file__))
     
     os.getcwd()
-    csv_path = os.path.join(os.getcwd(),"..", "..", "data", csv_name)
+    csv_path = os.path.normpath(os.path.join(base_path, "..", "..", "data", csv_name))
     
     if(encoding):
         df = pd.read_csv(csv_path, encoding=encoding)
     else:
         df = pd.read_csv(csv_path)
+
+    if(setIndexName):
+        df.set_index(setIndexName, inplace=True)        
     
     return df

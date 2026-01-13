4a8144e1285ebe7c99cf14458cabd6e8602a49aa
Giacomo Da Re <giacomodare98@gmail.com>
2026-01-13 16:33:06 +0100

    Update generic data reader. Set **kwargs argument for read_csv.

--- utilities/reader_tools/GenericDatasetReader.py
+++ utilities/reader_tools/GenericDatasetReader.py
@@ -1,21 +1,19 @@
 import os
 import pandas as pd
 
-def DataframeFromCSV(csv_name, encoding=None, setIndexName=None):
+def DataframeFromCSV(csv_name, setIndexName=None, **kwargs):
     base_path = os.path.dirname(os.path.abspath(__file__))
-    
+
     os.getcwd()
     csv_path = os.path.normpath(os.path.join(base_path, "..", "..", "data", csv_name))
-    
-    if(encoding):
-        df = pd.read_csv(csv_path, encoding=encoding)
-    else:
-        df = pd.read_csv(csv_path)
 
-    if(setIndexName):
-        df.set_index(setIndexName, inplace=True)        
+    df = pd.read_csv(csv_path, **kwargs)
     
+    if(setIndexName):
+        df.set_index(setIndexName, inplace=True)
+        
     return df
+
 
 def DataframeFromExcel(excel_name, setIndexName=None):
     base_path = os.path.dirname(os.path.abspath(__file__))
